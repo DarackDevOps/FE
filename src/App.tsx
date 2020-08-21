31266d8a126d82dc46dec3d0a0968b7bf84f8ef8
suyeon <77sy777@gmail.com>
2020-08-21 16:17:35 +0900

    layout js converted to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import Header from "./components/common/Header";
+import Footer from "./components/common/Footer";
+import "./App.css";
+import { Route } from "react-router-dom";
+import Business from "./pages/Business";
+import Map from "./pages/Map";
+import Board from "./pages/Board";
+import Qanda from "./pages/Qanda";
+import Main from "./pages/Main";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      {/* spa 가 아닌 링크타고 넘어가는 걸로 바꾸자. */}
+      <div>
+        <Route component={Main} exact path="/" />
+        <Route component={Business} path="/business" />
+        <Route component={Map} path="/map" />
+        <Route component={Board} path="/board" />
+        <Route component={Qanda} path="/qanda" />
+      </div>
+      {/* 메인 페이지 구성. */}
+
+      <Footer />
+    </>
   );
 }
 
---
+++ src/pages/Qanda.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Qanda = () => {
+  return <div>Qanda</div>;
+};
+export default Qanda;
