46712785b93a403e3919a4492dc8a8084b1b3ff7
hyun99999 <ypsophila97@icloud.com>
2020-08-25 04:34:24 +0900

    page/business first commit

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import Fade from 'react-reveal/Fade';
+
 const Photo = styled.img`
   width: auto;
   height: auto;
   max-width: 250px;
   max-height: 250px;
-  margin: 30px;
+  margin-left: 20px;
 `;
 const PhotoBlock = styled.div`
   margin: 0 auto;
@@ -16,11 +18,14 @@ const PhotoBlock = styled.div`
 const Main = () => {
   return (
     <>
-      <div>
+      <Fade left>
         <h3>SAMSUNG - SERVIECE CENTER.</h3>
-      </div>
+      </Fade>
 
       <PhotoBlock>
+        <Fade left>
+          <h3>SAMSUNG - SERVIECE CENTER.</h3>
+        </Fade>
         <Photo src={require('../images/1.jpg')} />
         <Photo src={require('../images/2.jpg')} />
         <h3>1,2번내용입니다.</h3>
@@ -36,6 +41,17 @@ const Main = () => {
       <h3>3,4번내용입니다.</h3>
 
       <h3>3,4번내용입니다.</h3>
+
+      <Fade left>
+        <section>
+          <h3>SAMSUNG - SERVIECE CENTER.</h3>
+        </section>
+      </Fade>
+      <Fade left>
+        <section>
+          <h3>SAMSUNG - SERVIECE CENTER.</h3>
+        </section>
+      </Fade>
     </>
   );
 };
