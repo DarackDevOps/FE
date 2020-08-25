468466aa1738ad41e6bfdcdc8303c20d565a66dd
pondsuyeon <pondsuyeon@gmail.com>
2020-08-25 15:06:38 +0900

    kakao map api

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,16 +1,74 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
+import '../../index.css';
 
 const FooterBlock = styled.div`
   bottom: 0;
   left: 0;
   width: 100%;
-  background: #868e96;
+  background: #ffffff;
+  //display: flex;
+  justify-content: center;
+  font-family: 'Alata', sans-serif;
+  //padding: 30px;
+  margin: 50px;
+  font-size: 17px;
 `;
+const Up = styled.div`
+  display: flex;
+  //margin: 30px;
+  //padding: 30px;
+`;
+const Right = styled.div`
+  margin-right: 100px;
+`;
+
+const Down = styled.div`
+  margin-top: 50px;
+`;
+
+const FontBule = styled.div`
+  color: blue;
+`;
+const FontGray = styled.div`
+  color: gray;
+`;
+
 const Footer = () => {
   return (
     <FooterBlock>
-      <h2>footer입니다.</h2>
+      <FontBule>VISIT US</FontBule>
+      <Up>
+        <Right>
+          <FontBule>WEST VILLAGE (NYC)</FontBule>
+          <FontGray>28 GREENWICH AVE</FontGray>
+          <FontGray> NEW YORK, NY 10011</FontGray>
+          <FontGray>212.498.9810</FontGray>
+        </Right>
+        <Right>
+          <FontBule>MIDTOWN (NYC)</FontBule>
+          <FontGray>COMING SOON</FontGray>
+          <FontGray>601LEXINGTON AVENUE</FontGray>
+          <FontGray>NEW YORK, NY 10022</FontGray>
+        </Right>
+        <Right>
+          <FontBule>WASHIGNTON DC</FontBule>
+          <FontGray>MAH-ZE-DAHR BAKERY AT GASTSBY</FontGray>
+          <FontGray>COMING SOON</FontGray>
+          <FontGray>1201 HALF STREET SE,</FontGray>
+          <FontGray>WASHINGTON DC 20003</FontGray>
+        </Right>
+      </Up>
+      <Down>
+        <div>
+          <FontBule>© 2020 MAHZEDAHR BAKERY LLC BRANDING &</FontBule>
+          <FontGray>BRANDING & CREATIVE DIRECTION: J’AIME COHEN</FontGray>
+          <FontGray>
+            PHOTOGRAPHY: KATE MATHIS, STEPHEN KENT JOHNSON, AND VANESSA REES
+          </FontGray>
+          <FontGray>WEBSITE DESIGN & DEVELOPMENT: W&CO</FontGray>
+        </div>
+      </Down>
     </FooterBlock>
   );
 };
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,11 +1,14 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
+
+import Fade from 'react-reveal/Fade';
+
 const Photo = styled.img`
   width: auto;
   height: auto;
   max-width: 250px;
   max-height: 250px;
-  margin: 30px;
+  margin-left: 20px;
 `;
 const PhotoBlock = styled.div`
   margin: 0 auto;
@@ -15,18 +18,40 @@ const PhotoBlock = styled.div`
 const Main = () => {
   return (
     <>
+      <Fade left>
+        <h3>SAMSUNG - SERVIECE CENTER.</h3>
+      </Fade>
+
       <PhotoBlock>
-        <Photo src={require("../images/1.jpg")} />
-        <Photo src={require("../images/2.jpg")} />
+        <Fade left>
+          <h3>SAMSUNG - SERVIECE CENTER.</h3>
+        </Fade>
+        <Photo src={require('../images/1.jpg')} />
+        <Photo src={require('../images/2.jpg')} />
         <h3>1,2번내용입니다.</h3>
       </PhotoBlock>
       <PhotoBlock>
-        <Photo src={require("../images/3.jpg")} />
-        <Photo src={require("../images/4.jpg")} />
+        <Photo src={require('../images/3.jpg')} />
+        <Photo src={require('../images/4.jpg')} />
         <div>
           <h3>3,4번내용입니다.</h3>
         </div>
       </PhotoBlock>
+
+      <h3>3,4번내용입니다.</h3>
+
+      <h3>3,4번내용입니다.</h3>
+
+      <Fade left>
+        <section>
+          <h3>SAMSUNG - SERVIECE CENTER.</h3>
+        </section>
+      </Fade>
+      <Fade left>
+        <section>
+          <h3>SAMSUNG - SERVIECE CENTER.</h3>
+        </section>
+      </Fade>
     </>
   );
 };
