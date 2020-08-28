62389cf3c36ac19a63875e22de1b367f6e91f4a2
Seona <bsa14523@gmail.com>
2020-08-28 20:55:09 +0900

    would be uploaded

--- servers/server.js
+++ servers/server.js
@@ -1,15 +1,27 @@
 const express = require('express');
-const app = express();
+
 const bodyParser = require('body-parser');
 const index = require('./routes/index');
-const dbTest = require('./routes/dbTest');
+const board = require('./routes/boardDb');
 const port = process.env.PORT || 3001;
+
+const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", `http://localhost:3000`);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use('/api', index);
-app.use('/dbTest', dbTest);
+app.use('/dbTest', board);
+
 
 app.listen(port, () => {
   console.log(`express is running on ${port}`);
--- src/pages/Write.tsx
+++ src/pages/Write.tsx
@@ -1,15 +1,12 @@
-import React, { useState } from "react";
-//import ReactDOM from "react-dom";
+import React from "react";
 import styled from "styled-components";
 import { Link } from 'react-router-dom';
-//import { useForm } from "react-hook-form";
-import Button from "../components/common/Button"
 import Header from '../components/board/Header';
+import Form from '../components/board/Form';
 
 /**
- * styled component 정의
- * 대충 빠르게 익히고 적응함
- * 나중에 진짜 하게 되면 공부하겠지 뭐 
+ * 함수, 인터페이스 정의
+ * props, state 사용법 알기
  */
 const MainDiv = styled.div`
     display: block;
@@ -19,124 +16,13 @@ const MainDiv = styled.div`
     border-collapse: collapse;
 `;
 
-const Div = styled.div`
-    text-align: left;
-    padding-left: 1%;
-    padding-top: 1%;
-    padding-bottom: 1%;
-    border-bottom: 1px solid #444444;
-`;
-
-const Label = styled.label`
-    margin-left: 1%;
-    margin-right: 2%;
-    width: 20%;
-`;
-
-const WriteLabel = styled(Label)`
-    position: relative;
-    bottom: 85px;
-    align-items: center;
-    vertical-align: middle;
-`;
-
-const Input = styled.input`
-    margin-left: 1%;
-    margin-right: 2%;
-    border: none;
-    width: 35%;
-`;
-
-const WriteInput = styled.textarea`
-    margin-left: 1%;
-    margin-right: 2%;
-    border: none;
-    width: 85%;
-    height: 200px;
-    resize: none;
-`;
-
-const ButtonDiv = styled.div`
-    margin-left: 45%;
-    margin-top: 1%;
-`;
-
-const BButton = styled(Button)`
-    margin: 1%;
-`;
-
-/**
- * 함수, 인터페이스 정의
- * props, state 사용법 알기
- */
-interface SetData {
-    onSubmit: ( form: {
-        title: string;
-        name: string;
-        visit_center: string;
-        contents: string;
-        }) => object;
-};
-
-interface FixData {
-    id: string;
-    date: Date | string | number;
-};
-
-const Write = ({ onSubmit }: SetData) => {
-    const [form, setForm] = useState({
-        title: '',
-        name: '',
-        visit_center: '',
-        contents: ''
-    });
-
-    const { title, name, visit_center, contents } = form;
-
-    const handleChange = ( event : React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement> ) => {
-        const { name, value } = event.target;
-        setForm({
-            ...form,
-            [name]: value
-        });
-    };
-    
-    const handleSubmit = (event : React.ChangeEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        onSubmit(form);
-        console.log(form);
-        setForm({
-            title: '',
-            name: '',
-            visit_center: '',
-            contents: ''
-        });
-    };
-
+const Write = () => {
+   
     return (
         <>
         <Header />
         <MainDiv>
-            <form>
-                <Div>
-                    <Label>제목</Label>
-                    <Input name="title" value={title} onChange= {handleChange}/>
-                </Div>
-                <Div>
-                    <Label>이름</Label>
-                    <Input name="name" value={name} onChange= {handleChange}/>
-                    <Label>방문센터</Label>
-                    <Input name="visit_center" value={visit_center} onChange= {handleChange}/>
-                </Div>
-                <Div>
-                   <WriteLabel>내용</WriteLabel>
-                    <WriteInput name="contents" value={contents} onChange= {handleChange}></WriteInput>
-                </Div>
-                <ButtonDiv>
-                <Link to="/board"><BButton type="submit" onSubmit={handleSubmit}>등록</BButton></Link>
-                <Link to="/board"><BButton>취소</BButton></Link>
-            </ButtonDiv>
-            </form>
+           <Form />
         </MainDiv>
         </>
     );
